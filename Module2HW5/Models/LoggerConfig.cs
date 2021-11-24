6dea80db4bfd33eb17d89d01ec97655e7d09ba80
Dubrov Ruslan <Ruslan Dubrov>
2021-11-24 23:22:23 +0200

    Create models

---
+++ Module2HW5/Models/LoggerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2HW5.Models
+{
+    public class LoggerConfig
+    {
+        public int LineSeparator { get; set; }
+        public string TimeFormat { get; set; }
+        public string DirectoryPath { get; set; }
+        public string BackUpDirectotyPath { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+    }
+}
---
+++ Module2HW5/Models/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace Module2HW5.Models
+{
+    public class Result
+    {
+        public bool Status { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
