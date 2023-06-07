8c6110f6a0c28e05e171863cea5eeafe31aaac49
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-07 16:04:04 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Module2HW5.Services.Abstractions;
+using Module2HW5.Services;
+using Module2HW5;
+
+var serviceProvider = new ServiceCollection()
+                .AddTransient<IFileService, FileService>()
+                .AddTransient<IConfigService, ConfigService>()
+                .AddTransient<IConsoleNotificator, ConsoleNotificator>()
+                .AddTransient<INotificationService, NotificationService>()
+                .AddTransient<ILoggerService, LoggerService>()
+                .AddTransient<IActions, Actions>()
+                .AddTransient<Starter>()
+                .BuildServiceProvider();
+var start = serviceProvider.GetService<Starter>();
+start.Run();
